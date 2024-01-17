55872cf8607838f45c7e16441a2f9795e48f8ed1
Mark Eibes <mark.eibes@gmail.com>
2024-01-17 13:56:05 +0100

    Add attrs to textarea

--- src/Beta/DOM/HTML/Textarea.purs
+++ src/Beta/DOM/HTML/Textarea.purs
@@ -3,7 +3,7 @@ module Beta.DOM.HTML.Textarea (textarea) where
 import Prelude
 
 import Beta.DOM.Attributes (BaseAttributesR)
-import Beta.DOM.Internal (FFIComponent, createBuiltinElement)
+import Beta.DOM.Internal (FFIComponent_, createBuiltinElement_)
 
 -- | The `<textarea>` HTML element represents a multi-line plain-text editing control.
 -- |
@@ -19,10 +19,23 @@ import Beta.DOM.Internal (FFIComponent, createBuiltinElement)
 -- | |---------|--------|------------------|-------------|
 -- | | `cols`   | Int | 50 | Specifies the visible width |
 -- | | `rows`   | Int | 5 | Specifies the visible number of lines |
-textarea :: FFIComponent TextareaAttributes
-textarea = createBuiltinElement "textarea"
+textarea :: FFIComponent_ TextareaAttributes
+textarea = createBuiltinElement_ "textarea"
 
 type TextareaAttributes = BaseAttributesR
   ( cols :: Int
   , rows :: Int
+  , value :: String
+  , defaultValue :: String
+  , autoComplete :: String
+  , autoFocus :: Boolean
+  , disabled :: Boolean
+  , form :: String
+  , maxLength :: Int
+  , minLength :: Int
+  , name :: String
+  , placeholder :: String
+  , readOnly :: Boolean
+  , required :: Boolean
+  , wrap :: String
   )
