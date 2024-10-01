a4708b400b47cff6b7474e9d282444ac4b5c0159
Mark Eibes <mark.eibes@wolt.com>
2024-10-01 20:46:10 +0200

    Add onToggle to summary

--- src/Beta/DOM/HTML/Summary.purs
+++ src/Beta/DOM/HTML/Summary.purs
@@ -4,7 +4,9 @@ import Prelude
 
 import Beta.DOM.Attributes (BaseAttributes)
 import Beta.DOM.Internal (FFIComponent, createBuiltinElement)
+import React.Basic.Events (EventHandler)
 
+type SummaryProps = (onToggle :: EventHandler | BaseAttributes)
 -- | The `<summary>` HTML element is used as a summary, caption, or legend for the content of a `<details>` element.
 -- |
 -- | #### Example:
@@ -15,5 +17,5 @@ import Beta.DOM.Internal (FFIComponent, createBuiltinElement)
 -- |   , text "The long version of the details goes here."
 -- |   ]
 -- | ```
-summary :: FFIComponent BaseAttributes
+summary :: FFIComponent SummaryProps
 summary = createBuiltinElement "summary"
